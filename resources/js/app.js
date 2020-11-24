22b423a17b1eb3bb07aac527bcb4a7cfc4fb0424
BlancaCasado <bcasadoalvarez@gmail.com>
2020-11-25 00:54:52 +0100

    vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import router from './router';
 import 'bootstrap/dist/css/bootstrap.css'; // added new after above
 import 'bootstrap'; //added new after above
 
 new Vue({
     el: '#app',  // id #app from our welcome.blade.php where it will render
+    router,
     render: h => h(App)
 });
